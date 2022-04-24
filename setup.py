97dcc8c456d38682c8c3796541e05e4b1d18186c
imPDA <dimaxa7@mail.ru>
2022-04-24 23:44:18 +0300

    0.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 from proxy_harvester.proxy_harvesters import __version__
 
 setup(
-    name='proxy_harvesters',
+    name='ProxyHarvesters',
     version=__version__,
 
     url='https://github.com/imPDA/proxy_harvester',
