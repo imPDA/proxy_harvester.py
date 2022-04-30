4011e5875c8deb40afd218250772436f08db5d95
imPDA <dimaxa7@mail.ru>
2022-04-30 17:06:47 +0300

    0.3.dev5

--- proxy_harvester/database.py
+++ proxy_harvester/database.py
@@ -14,7 +14,7 @@ class ProxyDatabase:
         self._database: Dict[str, ProxyAddress] = self.open_from_file()
 
     @property
-    def data(self) -> Dict[str, ProxyAddress]::
+    def data(self) -> Dict[str, ProxyAddress]:
         return self._database
 
     def open_from_file(self) -> Dict[str, ProxyAddress]:
