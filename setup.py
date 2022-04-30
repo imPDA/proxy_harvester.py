4bdf6e07d71f58d16e2d23c03868f1916848a8a4
imPDA <dimaxa7@mail.ru>
2022-04-30 13:25:12 +0300

    0.3.dev

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name='ProxyHarvester',
     version='0.3.dev',
-    url='https://github.com/imPDA/proxy_harvester',
+    url='https://github.com/imPDA/proxy_harvester.py',
     author='imPDA',
     # packages=find_packages(),
     packages=['proxy_harvester'],
